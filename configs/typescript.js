7cb851c0ad69ab24f645bb6e1cbdf8cb1c6fc31e
azemetre <azemetre@gmail.com>
2023-04-22 11:22:32 -0400

    fix: removing nonexistent obj

--- configs/typescript.js
+++ configs/typescript.js
@@ -7,23 +7,26 @@ const plugins = [
   "eslint-config-prettier",
 ];
 
-module.exports = Object.assign({}, all, {
-  extends: [
-    "eslint:recommended",
-    "airbnb-base",
-    "airbnb-typescript/base",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:sonarjs/recommended",
-  ],
-  plugins: [],
-  rules: {
-    "eslint-no-undef": 0,
-    "eslint-no-shadow": 0,
-  },
-  overrides: [
-    {
-      files: "*.test.@(ts)",
-      plugins: [...plugins, "vitest"],
+module.exports = Object.assign(
+  {},
+  {
+    extends: [
+      "eslint:recommended",
+      "airbnb-base",
+      "airbnb-typescript/base",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:sonarjs/recommended",
+    ],
+    plugins: [],
+    rules: {
+      "eslint-no-undef": 0,
+      "eslint-no-shadow": 0,
     },
-  ],
-});
+    overrides: [
+      {
+        files: "*.test.@(ts)",
+        plugins: [...plugins, "vitest"],
+      },
+    ],
+  }
+);
