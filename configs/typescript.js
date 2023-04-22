ae35c4e4a0ee01b299f61e15cb1014b7a396ce50
azemetre <azemetre@gmail.com>
2023-04-22 13:19:50 -0400

    feat: adding base, tweaking structure of repo

--- configs/typescript.js
+++ configs/typescript.js
@@ -1,48 +1,36 @@
 "use strict";
 
-const plugins = [
-  "@typescript-eslint",
-  "eslint-config-prettier",
-  "import",
-  "promise",
-  "sonarjs",
-  "unicorn",
-];
+const plugins = ["@typescript-eslint"];
 
-module.exports = Object.assign(
-  {},
-  {
-    env: {
-      browser: false,
-      es2022: true,
-    },
-    parserOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-    },
-    extends: [
-      "eslint:recommended",
-      "airbnb-base",
-      "airbnb-typescript/base",
-      "plugin:@typescript-eslint/recommended",
-      "plugin:sonarjs/recommended",
-      "plugin:promise/recommended",
-      "plugin:unicorn/recommended",
-      "plugin:vitest-globals/recommended",
-    ],
-    plugins: [...plugins],
-    rules: {
-      "eslint-no-undef": 0,
-      "eslint-no-shadow": 0,
-    },
-    overrides: [
-      {
-        files: "*.test.@(ts)",
-        env: {
-          "vitest-globals": true,
-        },
-        plugins: [...plugins, "vitest"],
+module.exports = {
+  extends: require.resolve("./base.js"),
+  overrides: [
+    {
+      files: "*.ts",
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
       },
-    ],
-  }
-);
+      extends: [
+        "airbnb-base",
+        "airbnb-typescript/base",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/typescript",
+        "plugin:vitest-globals/recommended",
+      ],
+      plugins: [...plugins],
+      rules: {
+        "eslint-no-undef": 0,
+        "eslint-no-shadow": 0,
+      },
+    },
+    {
+      files: "*.test.@(ts)",
+      env: {
+        "vitest-globals": true,
+      },
+      plugins: [...plugins, "vitest"],
+    },
+  ],
+};
