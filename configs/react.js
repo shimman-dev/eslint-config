a7bc1ffdd22c78c5ef3a87ff0dd605df53ab1cc8
azemetre <azemetre@gmail.com>
2023-04-22 11:22:32 -0400

    fix: removing nonexistent obj

--- configs/react.js
+++ configs/react.js
@@ -11,47 +11,50 @@ const plugins = [
   "eslint-config-prettier",
 ];
 
-module.exports = Object.assign({}, all, {
-  globals: {
-    React: true,
-    JSX: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "airbnb",
-    "airbnb-typescript",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:sonarjs/recommended",
-    "plugin:testing-library/react",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-  ],
-  plugins: [...plugins],
-  settings: {
-    react: {
-      version: "detect",
+module.exports = Object.assign(
+  {},
+  {
+    globals: {
+      React: true,
+      JSX: true,
     },
-  },
-  rules: {
-    "react/no-unescaped-entities": 0,
-    "react/react-in-jsx-scope": 0,
-    "eslint-no-undef": 0,
-    "eslint-no-shadow": 0,
-  },
-  languageOptions: {
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
+    extends: [
+      "eslint:recommended",
+      "airbnb",
+      "airbnb-typescript",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:sonarjs/recommended",
+      "plugin:testing-library/react",
+      "plugin:react/recommended",
+      "plugin:react-hooks/recommended",
+    ],
+    plugins: [...plugins],
+    settings: {
+      react: {
+        version: "detect",
       },
     },
-  },
-  overrides: [
-    {
-      files: "*.test.@(ts|tsx)",
-      plugins: [...plugins, "vitest"],
+    rules: {
+      "react/no-unescaped-entities": 0,
+      "react/react-in-jsx-scope": 0,
+      "eslint-no-undef": 0,
+      "eslint-no-shadow": 0,
     },
-  ],
-});
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    overrides: [
+      {
+        files: "*.test.@(ts|tsx)",
+        plugins: [...plugins, "vitest"],
+      },
+    ],
+  }
+);
 
 // this is so the `languageOptions` property won't be warned in the new config system
 Object.defineProperty(module.exports, "languageOptions", { enumerable: false });
