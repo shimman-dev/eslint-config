d42afa444ee23105dcda3903722863b8c6e5f361
azemetre <azemetre@gmail.com>
2023-07-15 10:11:45 -0400

    fix: properly loading shareable configs

--- src/react.js
+++ src/react.js
@@ -2,43 +2,30 @@
 
 const extendDeps = [
   "airbnb",
-  "airbnb-typescript",
-  "plugin:@typescript-eslint/recommended",
-  "plugin:import/typescript",
   "plugin:react/recommended",
   "plugin:react-hooks/recommended",
+  "plugin:jsx-a11y/recommended",
   "eslint-config-prettier",
 ];
 
-const pluginDeps = [
-  "@typescript-eslint",
-  "html",
-  "jsx-a11y",
-  "react",
-  "react-hooks",
-];
+const pluginDeps = ["html"];
 
 module.exports = {
-  extends: require.resolve("./base.js"),
+  extends: ["./base", "./typescript", ...extendDeps],
+  plugins: [...pluginDeps],
+  settings: {
+    react: {
+      version: "18.0",
+    },
+  },
+  rules: {
+    "react/no-unescaped-entities": "off",
+    "react/react-in-jsx-scope": "off",
+  },
   overrides: [
     {
-      files: "*.(ts|tsx)",
-      extends: [...extendDeps],
-      plugins: [...pluginDeps],
+      files: ["*.test.tsx"],
       rules: {
-        "react/no-unescaped-entities": "off",
-        "react/react-in-jsx-scope": "off",
-        "eslint-no-undef": "off",
-        "eslint-no-shadow": "off",
-        "import/prefer-default-export": "off",
-        "unicorn/prevent-abbreviations": "off",
-        "unicorn/filename-case": "off",
-      },
-    },
-    {
-      files: "*.test.@(ts|tsx)",
-      rules: {
-        "react/react-in-jsx-scope": "off",
         "react/jsx-props-no-spreading": "off",
       },
     },
--- src/vitest.js
+++ src/vitest.js
@@ -1,12 +1,16 @@
 "use strict";
 
+const extendDeps = [
+  "plugin:vitest/recommended",
+  "plugin:testing-library/react",
+];
+
 module.exports = {
-  extends: require.resolve("./base.js"),
-  overrides: [
-    {
-      files: "*.test.@(ts|tsx)",
-      extends: ["plugin:vitest/recommended", "plugin:testing-library/react"],
-      plugins: ["vitest", "testing-library"],
-    },
-  ],
+  extends: ["./base", ...extendDeps],
+  rules: {
+    "vitest/consistent-test-it": ["error", { fn: "test" }],
+    "vitest/prefer-each": "error",
+    "vitest/prefer-expect-resolves": "error",
+    "vitest/consistent-test-it": ["error", { fn: "test" }],
+  },
 };
