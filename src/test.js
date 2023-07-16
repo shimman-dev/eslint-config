f4a421afa7b62794818aa4f3d50384d57b63964c
azemetre <azemetre@gmail.com>
2023-07-16 19:22:04 -0400

    feat(test): revising ruleset

--- src/test.js
+++ src/test.js
@@ -7,10 +7,15 @@ const extendDeps = [
 
 module.exports = {
   extends: ["./base", ...extendDeps],
-  rules: {
-    "vitest/consistent-test-it": ["error", { fn: "test" }],
-    "vitest/prefer-each": "error",
-    "vitest/prefer-expect-resolves": "error",
-    "vitest/consistent-test-it": ["error", { fn: "test" }],
-  },
+  overrides: [
+    {
+      files: ["*.test.tsx", "*.test.ts"],
+      rules: {
+        "vitest/consistent-test-it": ["error", { fn: "test" }],
+        "vitest/prefer-each": "error",
+        "vitest/prefer-expect-resolves": "error",
+        "vitest/consistent-test-it": ["error", { fn: "test" }],
+      },
+    },
+  ],
 };
